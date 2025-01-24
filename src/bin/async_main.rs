39e525c43ec965e4c97fb38d493558d6bdc3ca26
Kyle Schwartz <kyleschwartz2000@gmail.com>
2025-01-24 17:25:38 -0500

    migrate to esp32c3

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -1,85 +1,265 @@
 #![no_std]
 #![no_main]
 
-use diane::ESPTime;
-use embassy_executor::Spawner;
-use embedded_hal_bus::spi::ExclusiveDevice;
-use embedded_sdmmc::Mode;
-use embedded_sdmmc::SdCard;
-use embedded_sdmmc::VolumeManager;
-use esp_backtrace as _;
-use esp_hal::{
-    delay::Delay,
-    gpio::{Level, Output},
-    prelude::*,
-    spi::{
-        master::{Config, Spi},
-        SpiMode,
-    },
-    timer::timg::TimerGroup,
-};
-use esp_println::println;
+use core::u8;
 
-// Per DOS: file names are max: 8 name, 3 extension
-const FILE_NAME: &str = "CONFIG.YML";
+use aligned::A4;
+// use alloc::borrow::ToOwned;
+use alloc::string::ToString;
+use block_device_adapters::{BufStream, BufStreamError, StreamSlice};
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig;
+use embassy_executor::{task, Spawner};
+use embassy_sync::mutex::Mutex;
+use embassy_time::{Delay, Timer};
+use embedded_fatfs::FsOptions;
+use embedded_io_async::{Read, Write};
+use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
+use esp_hal::dma::{DmaRxBuf, DmaTxBuf};
+use esp_hal::gpio::Pin;
+use esp_hal::gpio::{AnyPin, Input, Pull};
+use esp_hal::i2s::master::{DataFormat, I2s, Standard};
+use esp_hal::reset::wakeup_cause;
+use esp_hal::rng::Rng;
+use esp_hal::rtc_cntl::sleep::{RtcioWakeupSource, WakeupLevel};
+use esp_hal::rtc_cntl::{reset_reason, Rtc, SocResetReason};
+use esp_hal::spi::master::SpiDmaBus;
+use esp_hal::sync::RawMutex;
+use esp_hal::{dma_buffers, gpio, Cpu};
+use esp_hal::{
+    gpio::{Level, Output},
+    spi::master::{Config, Spi},
+    time::RateExtU32,
+    timer::systimer::SystemTimer,
+};
+use esp_hal_embassy::main;
+use esp_println::println;
+use log::info;
+use mbr_nostd::{MasterBootRecord, PartitionTable};
+use sdspi::{sd_init, SdSpi};
+use substring::Substring;
+use uuid::Builder;
+
+extern crate alloc;
+
+// DMA buffer size
+const I2S_BYTES: usize = 4092;
+
+#[task]
+async fn button_pressed(pin: AnyPin) {
+    println!("WOW");
+    let mut button = Input::new(pin, Pull::Up);
+
+    loop {
+        button.wait_for_low().await;
+        let start = embassy_time::Instant::now();
+        info!("Button pressed!");
+
+        button.wait_for_high().await;
+        let elapsed = start.elapsed().as_millis();
+        info!("Button released! Press duration: {:?}", elapsed);
+    }
+}
+
+#[task]
+async fn record() {}
 
 #[main]
 async fn main(_spawner: Spawner) {
-    let peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::max();
-        config
-    });
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let mut peripherals = esp_hal::init(config);
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_hal_embassy::init(timg0.timer0);
+    esp_alloc::heap_allocator!(72 * 1024);
 
     esp_println::logger::init_logger_from_env();
 
-    let sclk = peripherals.GPIO18;
-    let miso = peripherals.GPIO19;
-    let mosi = peripherals.GPIO23;
-    let cs = Output::new(peripherals.GPIO5, Level::Low);
+    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(timer0.alarm0);
 
-    let spi = Spi::new_with_config(
+    info!("Embassy initialized!");
+
+    let reason = reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
+
+    let wake_reason = wakeup_cause();
+    // wake_reason();
+
+    info!("{:?}", reason);
+    info!("{:?}", wake_reason);
+
+    let mut led = Output::new(peripherals.GPIO9, Level::High);
+    led.set_low();
+
+    // let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
+    // let _init = esp_wifi::init(
+    //     timer1.timer0,
+    //     esp_hal::rng::Rng::new(peripherals.RNG),
+    //     peripherals.RADIO_CLK,
+    // )
+    // .unwrap();
+
+    // -------------------
+    // SD Card - Device Setup
+    // -------------------
+
+    let dma_channel_spi = peripherals.DMA_CH0;
+    let (rx_buffer_spi, rx_descriptors_spi, tx_buffer_spi, tx_descriptors_spi) =
+        dma_buffers!(8 * I2S_BYTES);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors_spi, rx_buffer_spi).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors_spi, tx_buffer_spi).unwrap();
+
+    let sclk = peripherals.GPIO4;
+    let miso = peripherals.GPIO5;
+    let mosi = peripherals.GPIO6;
+    let mut cs = Output::new(peripherals.GPIO7, Level::Low);
+
+    let spi = Spi::new(
         peripherals.SPI2,
-        Config {
-            frequency: 100.kHz(),
-            mode: SpiMode::Mode0,
-            ..Config::default()
-        },
+        Config::default()
+            .with_frequency(400.kHz())
+            .with_mode(esp_hal::spi::Mode::_0),
     )
+    .unwrap()
     .with_sck(sclk)
     .with_mosi(mosi)
-    .with_miso(miso);
+    .with_miso(miso)
+    .with_dma(dma_channel_spi)
+    .into_async();
 
-    let delay = Delay::new();
+    let mut spi = SpiDmaBus::new(spi, dma_rx_buf, dma_tx_buf);
 
-    let spi_dev = ExclusiveDevice::new(spi, cs, delay).unwrap();
-    let sd_card = SdCard::new(spi_dev, delay);
+    loop {
+        match sd_init(&mut spi, &mut cs).await {
+            Ok(_) => break,
+            Err(e) => {
+                log::warn!("Sd init error: {:?}", e);
+                Timer::after_millis(10).await;
+            }
+        }
+    }
 
-    println!("Size of the sd card: {:#?}", sd_card.num_bytes().unwrap());
+    println!("sd_init complete");
 
-    let mut volume_manager = VolumeManager::new(sd_card, ESPTime::new());
+    let spi_bus = Mutex::<RawMutex, _>::new(spi);
+    let spid = SpiDeviceWithConfig::new(&spi_bus, cs, Config::default());
+    let mut sd = SdSpi::<_, _, A4>::new(spid, Delay);
 
-    let mut volume = volume_manager
-        .open_volume(embedded_sdmmc::VolumeIdx(0))
+    while sd.init().await.is_err() {
+        println!("Failed to init card, retrying...");
+        Timer::after_millis(500).await;
+    }
+    // Increase the speed up to the SD max of 25mhz
+    let mut config = Config::default();
+    config.frequency = 25.MHz();
+    sd.spi().set_config(config);
+
+    let size = sd.size().await;
+    println!("Initialization complete! Got card with size {:?}", size);
+
+    // -------------------
+    // i2s Setup
+    // -------------------
+
+    let bclk = peripherals.GPIO0; // sck - purple
+    let din = peripherals.GPIO2; //  sd  - black
+    let ws = peripherals.GPIO1; //   ws  - blue
+
+    let dma_channel_i2s = peripherals.DMA_CH1;
+    let (rx_buffer_i2s, rx_descriptors_i2s, _, tx_descriptors_i2s) = dma_buffers!(4 * I2S_BYTES, 0);
+
+    let i2s = I2s::new(
+        peripherals.I2S0,
+        Standard::Philips,
+        DataFormat::Data16Channel16,
+        8.kHz(),
+        dma_channel_i2s,
+        rx_descriptors_i2s,
+        tx_descriptors_i2s,
+    )
+    .into_async();
+
+    let i2s_rx = i2s.i2s_rx.with_bclk(bclk).with_ws(ws).with_din(din).build();
+
+    let mut transfer = i2s_rx.read_dma_circular_async(rx_buffer_i2s).unwrap();
+    let mut data = [0u8; I2S_BYTES];
+
+    // -------------------
+    // Record & Write Data
+    // -------------------
+
+    let mut rng = Rng::new(peripherals.RNG);
+    let mut bytes = [0u8; 16];
+    for i in 0..4 {
+        let random_bytes = rng.random().to_ne_bytes();
+        bytes[i * 4..(i + 1) * 4].copy_from_slice(&random_bytes);
+    }
+
+    let uuid = Builder::from_random_bytes(bytes)
+        .into_uuid()
+        .to_string()
+        .substring(0, 8)
+        .to_string();
+    let file_name = uuid + &".WAV";
+
+    // https://github.com/AlexCharlton/mpfs-hal/blob/main/examples/src/bin/embassy-sd.rs
+
+    let mut inner = BufStream::<_, 512>::new(sd);
+    let mut buf = [0; 512];
+    inner.read(&mut buf).await.unwrap();
+    let mbr = MasterBootRecord::from_bytes(&buf).unwrap();
+    println!("MBR: {:?}\n", mbr.partition_table_entries());
+
+    let partition = mbr.partition_table_entries()[0];
+    let start_offset = partition.logical_block_address as u64 * 512;
+    let end_offset = start_offset + partition.sector_count as u64 * 512;
+    let inner = StreamSlice::new(inner, start_offset, end_offset)
+        .await
         .unwrap();
-    println!("Volume 0: {:?}", volume);
 
-    let mut root_dir = volume.open_root_dir().unwrap();
+    let mut button = Input::new(peripherals.GPIO10, Pull::Up);
+    // _spawner
+    //     .spawn(button_pressed(peripherals.GPIO10.degrade()))
+    //     .unwrap();
 
-    let mut file = root_dir
-        .open_file_in_dir(FILE_NAME, Mode::ReadWriteCreate)
-        .unwrap();
-    let contents = b"
-wifi:
-    SSID: \"\"
-    PASSWORD: \"\"
-    ";
-    file.write(contents).unwrap();
+    async {
+        let fs = embedded_fatfs::FileSystem::new(inner, FsOptions::new())
+            .await
+            .unwrap();
+        {
+            let mut f = fs.root_dir().create_file(file_name.as_str()).await.unwrap();
 
-    file.close().unwrap();
+            led.set_high();
 
-    println!("File written!");
+            loop {
+                let i2s_bytes_read = transfer.pop(&mut data).await.unwrap();
+
+                f.write(&data[..i2s_bytes_read]).await.unwrap();
+                f.flush().await.unwrap();
+
+                if button.is_low() {
+                    // Debounce
+                    button.wait_for_high().await;
+                    break;
+                }
+            }
+        }
+
+        fs.unmount().await.unwrap();
+
+        Ok::<(), embedded_fatfs::Error<BufStreamError<sdspi::Error>>>(())
+    }
+    .await
+    .expect("Filesystem tests failed!");
+
+    log::info!("Out of the loop!");
+
+    led.set_low();
+
+    // button.wakeup_enable(true, WakeEvent::LowLevel);
+    let wakeup_pins: &mut [(&mut dyn gpio::RtcPinWithResistors, WakeupLevel)] =
+        &mut [(&mut peripherals.GPIO3, WakeupLevel::Low)];
+
+    let wake_source = RtcioWakeupSource::new(wakeup_pins);
+
+    let mut rtc = Rtc::new(peripherals.LPWR);
+    rtc.sleep_deep(&[&wake_source])
 }

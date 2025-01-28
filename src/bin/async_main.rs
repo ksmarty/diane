830ebe0090afb91143da1d25089e3cb372915138
Kyle Schwartz <kyleschwartz2000@gmail.com>
2025-01-28 18:51:46 -0500

    remove unneccessary code

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -4,26 +4,20 @@
 use core::u8;
 
 use aligned::A4;
-use alloc::string::ToString;
 use block_device_adapters::{BufStream, BufStreamError, StreamSlice};
 use embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
 use embassy_sync::mutex::Mutex;
 use embassy_time::{Delay, Timer};
 use embedded_fatfs::{FileSystem, FsOptions};
-use embedded_io::SeekFrom;
-use embedded_io_async::{Read, Seek, Write};
+use embedded_io_async::{Read, Write};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::dma::{DmaRxBuf, DmaTxBuf};
-use esp_hal::gpio::{Input, Pull};
+use esp_hal::dma_buffers;
 use esp_hal::i2s::master::{DataFormat, I2s, Standard};
-use esp_hal::reset::wakeup_cause;
-use esp_hal::rtc_cntl::sleep::{RtcioWakeupSource, WakeupLevel};
-use esp_hal::rtc_cntl::{reset_reason, Rtc, SocResetReason};
 use esp_hal::spi::master::SpiDmaBus;
 use esp_hal::sync::RawMutex;
-use esp_hal::{dma_buffers, gpio, Cpu};
 use esp_hal::{
     gpio::{Level, Output},
     spi::master::{Config, Spi},
@@ -31,8 +25,7 @@ use esp_hal::{
     timer::systimer::SystemTimer,
 };
 use esp_hal_embassy::main;
-use esp_println::{print, println};
-use log::info;
+use esp_println::println;
 use mbr_nostd::{MasterBootRecord, PartitionTable};
 use sdspi::{sd_init, SdSpi};
 
@@ -44,7 +37,7 @@ const I2S_BYTES: usize = 4092;
 #[main]
 async fn main(_spawner: Spawner) {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let mut peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(72 * 1024);
 
@@ -53,24 +46,29 @@ async fn main(_spawner: Spawner) {
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
 
-    info!("Embassy initialized!");
+    // -------------------
+    // i2s Setup
+    // -------------------
 
-    let reason = reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
+    let bclk = peripherals.GPIO0;
+    let din = peripherals.GPIO2;
+    let ws = peripherals.GPIO1;
 
-    let wake_reason = wakeup_cause();
+    let dma_channel_i2s = peripherals.DMA_CH1;
+    let (rx_buffer_i2s, rx_descriptors_i2s, _, tx_descriptors_i2s) = dma_buffers!(4 * I2S_BYTES, 0);
 
-    info!("{:?}", reason);
-    info!("{:?}", wake_reason);
+    let i2s = I2s::new(
+        peripherals.I2S0,
+        Standard::Philips,
+        DataFormat::Data16Channel16,
+        16.kHz(),
+        dma_channel_i2s,
+        rx_descriptors_i2s,
+        tx_descriptors_i2s,
+    )
+    .into_async();
 
-    let mut led = Output::new(peripherals.GPIO9, Level::Low);
-
-    // let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
-    // let _init = esp_wifi::init(
-    //     timer1.timer0,
-    //     esp_hal::rng::Rng::new(peripherals.RNG),
-    //     peripherals.RADIO_CLK,
-    // )
-    // .unwrap();
+    let i2s_rx = i2s.i2s_rx.with_bclk(bclk).with_ws(ws).with_din(din).build();
 
     // -------------------
     // SD Card - Device Setup
@@ -130,32 +128,6 @@ async fn main(_spawner: Spawner) {
     let size = sd.size().await;
     println!("Initialization complete! Got card with size {:?}", size);
 
-    // -------------------
-    // i2s Setup
-    // -------------------
-
-    let bclk = peripherals.GPIO0; // sck - purple
-    let din = peripherals.GPIO2; //  sd  - black
-    let ws = peripherals.GPIO1; //   ws  - blue
-
-    let dma_channel_i2s = peripherals.DMA_CH1;
-    let (rx_buffer_i2s, rx_descriptors_i2s, _, tx_descriptors_i2s) = dma_buffers!(4 * I2S_BYTES, 0);
-
-    let i2s = I2s::new(
-        peripherals.I2S0,
-        Standard::Philips,
-        DataFormat::Data16Channel16,
-        16.kHz(),
-        dma_channel_i2s,
-        rx_descriptors_i2s,
-        tx_descriptors_i2s,
-    )
-    .into_async();
-
-    let i2s_rx = i2s.i2s_rx.with_bclk(bclk).with_ws(ws).with_din(din).build();
-
-    // https://github.com/AlexCharlton/mpfs-hal/blob/main/examples/src/bin/embassy-sd.rs
-
     let mut inner = BufStream::<_, 512>::new(sd);
     let mut buf = [0; 512];
     inner.read(&mut buf).await.unwrap();
@@ -169,60 +141,18 @@ async fn main(_spawner: Spawner) {
         .await
         .unwrap();
 
-    let mut button = Input::new(peripherals.GPIO10, Pull::Up);
-
     async {
         let fs = FileSystem::new(inner, FsOptions::new()).await.unwrap();
 
         {
             let root_dir = fs.root_dir();
-            let recordings = match root_dir.open_dir("recordings").await {
-                Ok(v) => v,
-                Err(_) => root_dir.create_dir("recordings").await.unwrap(),
-            };
-            let mut iter = recordings.iter();
 
-            let mut highest_number = 0;
-            while let Some(r) = iter.next().await {
-                let e = r.unwrap();
-                let name = e.file_name();
-                let number = name
-                    .split(".")
-                    .nth(0)
-                    .and_then(|n| n.parse::<u32>().ok())
-                    .unwrap_or(0);
-                highest_number = highest_number.max(number);
-            }
-
-            let file_name = (highest_number + 1).to_string() + &".wav";
-
-            let mut f = recordings.create_file(file_name.as_str()).await.unwrap();
-
-            const HEADER_SIZE: usize = 44;
-            let header: [u8; HEADER_SIZE] = [
-                b'R', b'I', b'F', b'F', // ChunkID
-                0xff, 0xff, 0xff, 0xff, // ChunkSize (to be filled)
-                b'W', b'A', b'V', b'E', // Format
-                b'f', b'm', b't', b' ', // Subchunk1ID
-                16, 0, 0, 0, // Subchunk1Size (16 for PCM)
-                1, 0, // AudioFormat (1 = PCM)
-                2, 0, // NumChannels (1 = Mono)
-                0x80, 0x3E, 0x00, 0x00, // SampleRate (16 kHz)
-                0x00, 0xFA, 0x00, 0x00, // ByteRate
-                4, 0, // BlockAlign
-                16, 0, // BitsPerSample
-                b'd', b'a', b't', b'a', // Subchunk2ID
-                0xff, 0xff, 0xff, 0xff, // Subchunk2Size (to be filled)
-            ];
-            f.write_all(&header).await.unwrap();
-            f.flush().await.unwrap();
-
-            led.set_high();
+            let mut f = root_dir.create_file("example.wav").await.unwrap();
 
             let mut data = [0u8; I2S_BYTES];
             let mut transfer = i2s_rx.read_dma_circular_async(rx_buffer_i2s).unwrap();
 
-            // 1026 garbage bytes at the start bytes
+            let mut iters = 0;
 
             loop {
                 let i2s_bytes_read = transfer.pop(&mut data).await.unwrap();
@@ -230,26 +160,12 @@ async fn main(_spawner: Spawner) {
                 f.write(&data[..i2s_bytes_read]).await.unwrap();
                 f.flush().await.unwrap();
 
-                if button.is_low() {
-                    // Debounce
-                    button.wait_for_high().await;
+                iters += 1;
+
+                if iters > 100 {
                     break;
                 }
             }
-
-            let file_size: u32 = f.stream_position().await.unwrap() as u32;
-
-            f.seek(SeekFrom::Start(4)).await.unwrap();
-            f.write_all(&(file_size - 8).to_ne_bytes()).await.unwrap();
-            f.flush().await.unwrap();
-
-            f.seek(SeekFrom::Start(40)).await.unwrap();
-            f.write_all(&(file_size - HEADER_SIZE as u32).to_ne_bytes())
-                .await
-                .unwrap();
-            f.flush().await.unwrap();
-
-            println!("File size: {}", file_size);
         }
 
         fs.unmount().await.unwrap();
@@ -260,14 +176,4 @@ async fn main(_spawner: Spawner) {
     .expect("Filesystem tests failed!");
 
     log::info!("Out of the loop!");
-
-    led.set_low();
-
-    let wakeup_pins: &mut [(&mut dyn gpio::RtcPinWithResistors, WakeupLevel)] =
-        &mut [(&mut peripherals.GPIO3, WakeupLevel::Low)];
-
-    let wake_source = RtcioWakeupSource::new(wakeup_pins);
-
-    let mut rtc = Rtc::new(peripherals.LPWR);
-    rtc.sleep_deep(&[&wake_source])
 }

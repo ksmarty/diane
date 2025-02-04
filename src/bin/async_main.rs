981cf8565f5366e7eb2693c64bde87a3229641bf
Kyle Schwartz <kyleschwartz2000@gmail.com>
2025-02-04 17:26:06 -0500

    switch back to embeddedsdmmc

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -3,38 +3,28 @@
 
 use core::u8;
 
-use aligned::A4;
-use alloc::string::ToString;
-use block_device_adapters::{BufStream, BufStreamError, StreamSlice};
-use embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig;
+use diane::{enter_deep_sleep, ESPTime, SDUtils, HEADER, HEADER_SIZE};
 use embassy_executor::Spawner;
-use embassy_sync::mutex::Mutex;
-use embassy_time::{Delay, Timer};
-use embedded_fatfs::{FileSystem, FsOptions};
-use embedded_io::SeekFrom;
-use embedded_io_async::{Read, Seek, Write};
+use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_sdmmc::filesystem::ToShortFileName;
+use embedded_sdmmc::{Mode, SdCard, VolumeManager};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
-use esp_hal::dma::{DmaRxBuf, DmaTxBuf};
-use esp_hal::gpio::{Input, Pull};
+use esp_hal::gpio::{Input, Pin, Pull};
 use esp_hal::i2s::master::{DataFormat, I2s, Standard};
 use esp_hal::reset::wakeup_cause;
-use esp_hal::rtc_cntl::sleep::{RtcioWakeupSource, WakeupLevel};
-use esp_hal::rtc_cntl::{reset_reason, Rtc, SocResetReason};
-use esp_hal::spi::master::SpiDmaBus;
-use esp_hal::sync::RawMutex;
-use esp_hal::{dma_buffers, gpio, Cpu};
+use esp_hal::rtc_cntl::{reset_reason, SocResetReason};
 use esp_hal::{
+    delay::Delay,
     gpio::{Level, Output},
     spi::master::{Config, Spi},
     time::RateExtU32,
     timer::systimer::SystemTimer,
 };
+use esp_hal::{dma_buffers, Cpu};
 use esp_hal_embassy::main;
-use esp_println::{print, println};
+use esp_println::println;
 use log::info;
-use mbr_nostd::{MasterBootRecord, PartitionTable};
-use sdspi::{sd_init, SdSpi};
 
 extern crate alloc;
 
@@ -44,7 +34,7 @@ const I2S_BYTES: usize = 4092;
 #[main]
 async fn main(_spawner: Spawner) {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let mut peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(72 * 1024);
 
@@ -76,59 +66,35 @@ async fn main(_spawner: Spawner) {
     // SD Card - Device Setup
     // -------------------
 
-    let dma_channel_spi = peripherals.DMA_CH0;
-    let (rx_buffer_spi, rx_descriptors_spi, tx_buffer_spi, tx_descriptors_spi) =
-        dma_buffers!(8 * I2S_BYTES);
-    let dma_rx_buf = DmaRxBuf::new(rx_descriptors_spi, rx_buffer_spi).unwrap();
-    let dma_tx_buf = DmaTxBuf::new(tx_descriptors_spi, tx_buffer_spi).unwrap();
-
     let sclk = peripherals.GPIO4;
     let miso = peripherals.GPIO5;
     let mosi = peripherals.GPIO6;
-    let mut cs = Output::new(peripherals.GPIO7, Level::Low);
+    let cs = Output::new(peripherals.GPIO7, Level::Low);
 
     let spi = Spi::new(
         peripherals.SPI2,
         Config::default()
-            .with_frequency(400.kHz())
+            .with_frequency(25.MHz())
             .with_mode(esp_hal::spi::Mode::_0),
     )
     .unwrap()
     .with_sck(sclk)
     .with_mosi(mosi)
-    .with_miso(miso)
-    .with_dma(dma_channel_spi)
-    .into_async();
+    .with_miso(miso);
 
-    let mut spi = SpiDmaBus::new(spi, dma_rx_buf, dma_tx_buf);
+    let delay = Delay::new();
 
-    loop {
-        match sd_init(&mut spi, &mut cs).await {
-            Ok(_) => break,
-            Err(e) => {
-                log::warn!("Sd init error: {:?}", e);
-                Timer::after_millis(10).await;
-            }
-        }
-    }
+    let spi_dev = ExclusiveDevice::new(spi, cs, delay).unwrap();
+    let sd_card = SdCard::new(spi_dev, delay);
 
-    println!("sd_init complete");
+    println!("Size of the sd card: {:#?}", sd_card.num_bytes().unwrap());
 
-    let spi_bus = Mutex::<RawMutex, _>::new(spi);
-    let spid = SpiDeviceWithConfig::new(&spi_bus, cs, Config::default());
-    let mut sd = SdSpi::<_, _, A4>::new(spid, Delay);
+    let mut volume_manager = VolumeManager::new(sd_card, ESPTime::new());
 
-    while let Err(err) = sd.init().await {
-        println!("Failed to init card: {:?}, retrying...", err);
-        Timer::after_millis(500).await;
-    }
-    // Increase the speed up to the SD max of 25mhz
-    let mut config = Config::default();
-    config.frequency = 25.MHz();
-    sd.spi().set_config(config);
-
-    let size = sd.size().await;
-    println!("Initialization complete! Got card with size {:?}", size);
+    let mut volume = volume_manager
+        .open_volume(embedded_sdmmc::VolumeIdx(0))
+        .unwrap();
+    println!("Volume 0: {:?}", volume);
 
     // -------------------
     // i2s Setup
@@ -139,7 +105,8 @@ async fn main(_spawner: Spawner) {
     let ws = peripherals.GPIO1; //   ws  - blue
 
     let dma_channel_i2s = peripherals.DMA_CH1;
-    let (rx_buffer_i2s, rx_descriptors_i2s, _, tx_descriptors_i2s) = dma_buffers!(4 * I2S_BYTES, 0);
+    let (mut rx_buffer_i2s, rx_descriptors_i2s, _, tx_descriptors_i2s) =
+        dma_buffers!(4 * I2S_BYTES, 0);
 
     let i2s = I2s::new(
         peripherals.I2S0,
@@ -149,125 +116,78 @@ async fn main(_spawner: Spawner) {
         dma_channel_i2s,
         rx_descriptors_i2s,
         tx_descriptors_i2s,
-    )
-    .into_async();
+    );
 
-    let i2s_rx = i2s.i2s_rx.with_bclk(bclk).with_ws(ws).with_din(din).build();
-
-    // https://github.com/AlexCharlton/mpfs-hal/blob/main/examples/src/bin/embassy-sd.rs
-
-    let mut inner = BufStream::<_, 512>::new(sd);
-    let mut buf = [0; 512];
-    inner.read(&mut buf).await.unwrap();
-    let mbr = MasterBootRecord::from_bytes(&buf).unwrap();
-    println!("MBR: {:?}\n", mbr.partition_table_entries());
-
-    let partition = mbr.partition_table_entries()[0];
-    let start_offset = partition.logical_block_address as u64 * 512;
-    let end_offset = start_offset + partition.sector_count as u64 * 512;
-    let inner = StreamSlice::new(inner, start_offset, end_offset)
-        .await
-        .unwrap();
+    let mut i2s_rx = i2s.i2s_rx.with_bclk(bclk).with_ws(ws).with_din(din).build();
 
     let mut button = Input::new(peripherals.GPIO10, Pull::Up);
 
+    let mut root_dir = volume.open_root_dir().unwrap();
+
+    let file_name = SDUtils::get_next_file_name(&mut root_dir);
+
+    let mut file = root_dir
+        .open_file_in_dir(
+            file_name.to_short_filename().unwrap(),
+            Mode::ReadWriteCreate,
+        )
+        .unwrap();
+
+    file.write(&HEADER).unwrap();
+
+    led.set_high();
+
+    let mut transfer = i2s_rx.read_dma_circular(&mut rx_buffer_i2s).unwrap();
+
+    let mut data = [0u8; 2 * I2S_BYTES];
+
+    // Ignore the first n buffers. Garbage data
+    let mut ignore_counter = 6;
+
     async {
-        let fs = FileSystem::new(inner, FsOptions::new()).await.unwrap();
+        loop {
+            let avail = transfer.available().unwrap();
 
-        {
-            let root_dir = fs.root_dir();
-            let recordings = match root_dir.open_dir("recordings").await {
-                Ok(v) => v,
-                Err(_) => root_dir.create_dir("recordings").await.unwrap(),
-            };
-            let mut iter = recordings.iter();
-
-            let mut highest_number = 0;
-            while let Some(r) = iter.next().await {
-                let e = r.unwrap();
-                let name = e.file_name();
-                let number = name
-                    .split(".")
-                    .nth(0)
-                    .and_then(|n| n.parse::<u32>().ok())
-                    .unwrap_or(0);
-                highest_number = highest_number.max(number);
+            if avail == 0 {
+                continue;
             }
 
-            let file_name = (highest_number + 1).to_string() + &".wav";
+            transfer.pop(&mut data[..avail]).unwrap();
 
-            let mut f = recordings.create_file(file_name.as_str()).await.unwrap();
-
-            const HEADER_SIZE: usize = 44;
-            let header: [u8; HEADER_SIZE] = [
-                b'R', b'I', b'F', b'F', // ChunkID
-                0xff, 0xff, 0xff, 0xff, // ChunkSize (to be filled)
-                b'W', b'A', b'V', b'E', // Format
-                b'f', b'm', b't', b' ', // Subchunk1ID
-                16, 0, 0, 0, // Subchunk1Size (16 for PCM)
-                1, 0, // AudioFormat (1 = PCM)
-                2, 0, // NumChannels (1 = Mono)
-                0x80, 0x3E, 0x00, 0x00, // SampleRate (16 kHz)
-                0x00, 0xFA, 0x00, 0x00, // ByteRate
-                4, 0, // BlockAlign
-                16, 0, // BitsPerSample
-                b'd', b'a', b't', b'a', // Subchunk2ID
-                0xff, 0xff, 0xff, 0xff, // Subchunk2Size (to be filled)
-            ];
-            f.write_all(&header).await.unwrap();
-            f.flush().await.unwrap();
-
-            led.set_high();
-
-            let mut data = [0u8; I2S_BYTES];
-            let mut transfer = i2s_rx.read_dma_circular_async(rx_buffer_i2s).unwrap();
-
-            // 1026 garbage bytes at the start bytes
-
-            loop {
-                let i2s_bytes_read = transfer.pop(&mut data).await.unwrap();
-
-                f.write(&data[..i2s_bytes_read]).await.unwrap();
-                f.flush().await.unwrap();
-
-                if button.is_low() {
-                    // Debounce
-                    button.wait_for_high().await;
-                    break;
-                }
+            if ignore_counter > 0 {
+                ignore_counter -= 1;
+                continue;
             }
 
-            let file_size: u32 = f.stream_position().await.unwrap() as u32;
+            file.write(&data[..avail]).unwrap();
+            file.flush().unwrap();
 
-            f.seek(SeekFrom::Start(4)).await.unwrap();
-            f.write_all(&(file_size - 8).to_ne_bytes()).await.unwrap();
-            f.flush().await.unwrap();
-
-            f.seek(SeekFrom::Start(40)).await.unwrap();
-            f.write_all(&(file_size - HEADER_SIZE as u32).to_ne_bytes())
-                .await
-                .unwrap();
-            f.flush().await.unwrap();
-
-            println!("File size: {}", file_size);
+            if button.is_low() {
+                // Debounce
+                button.wait_for_high().await;
+                break;
+            }
         }
 
-        fs.unmount().await.unwrap();
-
-        Ok::<(), embedded_fatfs::Error<BufStreamError<sdspi::Error>>>(())
+        log::info!("Done listening to mic!");
     }
-    .await
-    .expect("Filesystem tests failed!");
+    .await;
 
-    log::info!("Out of the loop!");
+    let file_size: u32 = file.length();
+
+    file.seek_from_start(4).unwrap();
+    file.write(&(file_size - 8).to_ne_bytes()).unwrap();
+    file.flush().unwrap();
+
+    file.seek_from_start(40).unwrap();
+    file.write(&(file_size - HEADER_SIZE as u32).to_ne_bytes())
+        .unwrap();
+    file.flush().unwrap();
+
+    println!("File size: {}", file_size);
+    file.close().unwrap();
 
     led.set_low();
 
-    let wakeup_pins: &mut [(&mut dyn gpio::RtcPinWithResistors, WakeupLevel)] =
-        &mut [(&mut peripherals.GPIO3, WakeupLevel::Low)];
-
-    let wake_source = RtcioWakeupSource::new(wakeup_pins);
-
-    let mut rtc = Rtc::new(peripherals.LPWR);
-    rtc.sleep_deep(&[&wake_source])
+    enter_deep_sleep(peripherals.GPIO3.degrade(), peripherals.LPWR);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,97 @@
 #![no_std]
+
+extern crate alloc;
+
+use alloc::string::{String, ToString};
+use embassy_time::Instant;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_sdmmc::{Directory, SdCard, TimeSource, Timestamp};
+use esp_hal::{
+    delay::Delay,
+    gpio::{self, AnyPin, Output},
+    rtc_cntl::{
+        sleep::{RtcioWakeupSource, WakeupLevel},
+        Rtc,
+    },
+    spi::master::Spi,
+    Blocking,
+};
+
+pub struct ESPTime {
+    start: Instant,
+}
+
+impl ESPTime {
+    pub fn new() -> ESPTime {
+        Self {
+            start: Instant::now(),
+        }
+    }
+}
+
+impl TimeSource for ESPTime {
+    fn get_timestamp(&self) -> embedded_sdmmc::Timestamp {
+        let now = &self.start.as_secs();
+        Timestamp {
+            year_since_1970: (now / 31536000) as u8,
+            zero_indexed_month: ((now / 2628000) % 12) as u8,
+            zero_indexed_day: ((now / 86400) % 30) as u8,
+            hours: ((now / 3600) % 24) as u8,
+            minutes: ((now / 60) % 60) as u8,
+            seconds: (now % 60) as u8,
+        }
+    }
+}
+
+pub struct SDUtils {}
+
+impl SDUtils {
+    pub fn get_next_file_name(
+        root_dir: &mut Directory<
+            '_,
+            SdCard<ExclusiveDevice<Spi<'_, Blocking>, Output<'_>, Delay>, Delay>,
+            ESPTime,
+            4,
+            4,
+            1,
+        >,
+    ) -> String {
+        let mut highest_number = 0;
+        root_dir
+            .iterate_dir(|e| {
+                let name = String::from_utf8(e.name.base_name().to_vec()).unwrap();
+                let number = name.parse::<u32>().ok().unwrap_or(0);
+                highest_number = highest_number.max(number);
+            })
+            .unwrap();
+
+        (highest_number + 1).to_string() + &".wav"
+    }
+}
+
+pub fn enter_deep_sleep(mut gpio3: AnyPin, lpwr: esp_hal::peripherals::LPWR) {
+    let wakeup_pins: &mut [(&mut dyn gpio::RtcPinWithResistors, WakeupLevel)] =
+        &mut [(&mut gpio3, WakeupLevel::Low)];
+
+    let wake_source = RtcioWakeupSource::new(wakeup_pins);
+
+    let mut rtc = Rtc::new(lpwr);
+    rtc.sleep_deep(&[&wake_source])
+}
+
+pub const HEADER_SIZE: usize = 44;
+pub const HEADER: [u8; HEADER_SIZE] = [
+    b'R', b'I', b'F', b'F', // ChunkID
+    0xff, 0xff, 0xff, 0xff, // ChunkSize (to be filled)
+    b'W', b'A', b'V', b'E', // Format
+    b'f', b'm', b't', b' ', // Subchunk1ID
+    16, 0, 0, 0, // Subchunk1Size (16 for PCM)
+    1, 0, // AudioFormat (1 = PCM)
+    2, 0, // NumChannels (1 = Mono)
+    0x80, 0x3E, 0x00, 0x00, // SampleRate (16 kHz)
+    0x00, 0xFA, 0x00, 0x00, // ByteRate
+    4, 0, // BlockAlign
+    16, 0, // BitsPerSample
+    b'd', b'a', b't', b'a', // Subchunk2ID
+    0xff, 0xff, 0xff, 0xff, // Subchunk2Size (to be filled)
+];
